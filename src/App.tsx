988ad05dbcfa95efaea1296614a34cd28ed542de
binaryhexa <gif.glses@gmail.com>
2025-10-08 23:55:31 +0700

    first init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-red-500 text-4xl text-center font-bold">
+          first init for TalentaID
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
